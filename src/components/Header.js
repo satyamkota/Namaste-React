07327c5fb4659921a487f2df2e5d95fbad29731c
satyamkota <satyamkota9595@gmail.com>
2023-04-29 22:51:28 +0530

    Chapter 13

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import Logo from "../assets/img/foodvilla.png";
 import { Link } from "react-router-dom";
 import useOnline from "../utils/useOnline";
+import UserContext from "../utils/UserContext";
 
 // SPA - Single Page Application???
 // Client Side Routing
@@ -16,6 +17,8 @@ const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const isOnline = useOnline();
+
+  const { user } = useContext(UserContext);
 
   return (
     <div className="flex justify-between bg-pink-50 shadow-lg sm:bg-blue-50 md:bg-yellow-50">
@@ -38,11 +41,12 @@ const Header = () => {
           </Link>
         </ul>
       </div>
-      <h1>{isOnline ? "✅" : "🔴"}</h1>
+      <h1 className="p-10">{isOnline ? "✅" : "🔴"}</h1>
+      <span className="p-10 font-bold text-red-900">{user.name}</span>
       {isLoggedIn ? (
         <button onClick={() => setIsLoggedIn(false)}>Logout</button>
       ) : (
-        <button onClick={() => setIsLoggedIn(true)}>Login</button>
+        <button className="pr-10" onClick={() => setIsLoggedIn(true)}>Login</button>
       )}
     </div>
   );
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,4 +1,6 @@
 import { IMG_CDN_URL } from "../contants";
+import { useContext } from "react";
+import UserContext from "../utils/UserContext";
 
 const RestrauntCard = ({
   name,
@@ -6,12 +8,16 @@ const RestrauntCard = ({
   cloudinaryImageId,
   lastMileTravelString,
 }) => {
+  const { user } = useContext(UserContext);
   return (
     <div className="w-56 p-2 m-2 shadow-lg bg-pink-50">
       <img src={IMG_CDN_URL + cloudinaryImageId} />
       <h2 className="font-bold text-xl">{name}</h2>
       <h3>{cuisines.join(", ")}</h3>
-      <h4>{lastMileTravelString} minutes</h4>
+      <h4>{lastMileTravelString}</h4>
+      <h5 className="font-bold">
+        {user.name} - {user.email}
+      </h5>
     </div>
   );
 };
